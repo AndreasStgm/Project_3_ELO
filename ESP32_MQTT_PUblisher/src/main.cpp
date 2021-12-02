3641ef622f89fd459db3707790c50fdff1b414c5
AndreasStgm <andreassteegmans@hotmail.com>
2021-12-02 10:25:59 +0100

    mini aanpassing voor mathias

--- ESP32_MQTT_PUblisher/src/main.cpp
+++ ESP32_MQTT_PUblisher/src/main.cpp
@@ -29,8 +29,8 @@ const char *wifi_password = "computer"; // pw invullen
 
 const char *mqtt_server = "192.168.137.103"; // IP van MQTT broker invullen 192.168.0.13
 const char *RFIDtag_topic = "RFIDtag";       // home/topic nog in te vullen
-const char *mqtt_username = "esp32";        // MQTT username invullen
-const char *mqtt_password = "esp32";        // MQTT pw invullen
+const char *mqtt_username = "esp32";         // MQTT username invullen
+const char *mqtt_password = "esp32";         // MQTT pw invullen
 const char *clientID = "client_home";        // MQTT client ID invullen
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,10 +38,9 @@ const char *VoiceRecognition_topic = "VoiceRecognition"; // mathias sub_test
 const char *FacialRecognition_topic = "FacialRecognition";
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-char* Topic;
-byte* buffer;
-boolean Rflag=false;
+char *Topic;
+byte *buffer;
+boolean Rflag = false;
 int r_len;
 boolean succes;
 
@@ -51,19 +50,19 @@ WiFiClient wifiClient;
 PubSubClient client(mqtt_server, /*LISTENER PORT BROKER INVULLEN INGEVAL ANDERE*/ 1883, wifiClient);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
-void callback(char* topic, byte* payload, unsigned int length) {
-   //Payload=[];
-   Topic=topic;
-   Rflag=true; //will use in main loop
-   r_len=length; //will use in main loop
-   debugSerial.print("length message received in callback= ");
-   debugSerial.println(length);
-    for (int i=0;i<length;i++) 
-    {
-    buffer[i]=payload[i];
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  //Payload=[];
+  Topic = topic;
+  Rflag = true;   //will use in main loop
+  r_len = length; //will use in main loop
+  debugSerial.print("length message received in callback= ");
+  debugSerial.println(length);
+  for (int i = 0; i < length; i++)
+  {
+    buffer[i] = payload[i];
     debugSerial.print((char)payload[i]);
-    }
-
+  }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -93,7 +92,7 @@ void connect_MQTT()
   {
     debugSerial.println("Connected to MQTT Broker!");
     /////////////////////////////////////////////////////////////////////////////////////////////////
-    //boolean succes = client.subscribe(FacialRecognition_topic);    // mathias subtest 
+    //boolean succes = client.subscribe(FacialRecognition_topic);    // mathias subtest
     // client.setCallback(callback);
     /////////////////////////////////////////////////////////////////////////////////////////////////
   }
@@ -115,45 +114,44 @@ void setup()
 
   debugSerial.begin(9600);
   commsSerial.begin(115200);
-  
-  client.setServer(mqtt_server,1883); //test 02/12
+
+  client.setServer(mqtt_server, 1883); //test 02/12
   client.setCallback(callback);
-  boolean succes = client.subscribe(FacialRecognition_topic);  
+  boolean succes = client.subscribe(FacialRecognition_topic);
   connect_MQTT();
   // client.subscribe(FacialRecognition_topic,2);
-  
 }
 
 void loop()
 {
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// client.subscribe(FacialRecognition_topic,2);
-//~ String msg = "test message";
-//~ client.publish(FacialRecognition_topic, String(msg).c_str());
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+  // client.subscribe(FacialRecognition_topic,2);
+  //~ String msg = "test message";
+  //~ client.publish(FacialRecognition_topic, String(msg).c_str());
 
-//~ delay(1000);
-client.loop();
-    
-if(Rflag)
-{
-  debugSerial.print("Message arrived in main loop[");
-  debugSerial.print(Topic);
-  debugSerial.print("] ");
-  debugSerial.print("message length = ");
-  debugSerial.print(r_len);
-  //Serial.print(Payload);
-  for (int i=0;i<r_len;i++) 
+  //~ delay(1000);
+  client.loop();
+
+  if (Rflag)
   {
-    debugSerial.print((char)buffer[i]);
+    debugSerial.print("Message arrived in main loop[");
+    debugSerial.print(Topic);
+    debugSerial.print("] ");
+    debugSerial.print("message length = ");
+    debugSerial.print(r_len);
+    //Serial.print(Payload);
+    for (int i = 0; i < r_len; i++)
+    {
+      debugSerial.print((char)buffer[i]);
+    }
+    debugSerial.println();
+    Rflag = false;
   }
-  debugSerial.println();
-  Rflag=false;
-}
-debugSerial.println(waarde sub: );
-debugSerial.println(succes);
-/////////////////////////////////////////////////////////////////////////////////////////////////  
-  
+  debugSerial.println("waarde sub: ");
+  debugSerial.println(succes);
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   if (commsSerial.available() > 0)
   {
     RFIDPayload receivedPayload = commsRead();
