ae2b96bd418ec39993722ed60a1f7b371c328cd5
ijmert <ijmertu@gmail.com>
2021-12-16 15:28:49 +0100

    update nano 33ble sesne logica
    logica voor wat moet gebeuren wanneer een naam binnenkomt op facial recog, etc.

--- Arduino Nano 33 BLE Sense Event CTRL/src/main.cpp
+++ Arduino Nano 33 BLE Sense Event CTRL/src/main.cpp
@@ -6,6 +6,7 @@
 #include <Scheduler.h>
 #include <SPI.h>
 
+
 //-----Function Declaration-----
 String RFID_Read();
 String RX_Handler();
@@ -19,6 +20,7 @@ bool stem_herkent = false;
 bool tag_herkent = false;
 String rfid_naam = "";
 String stem_naam = "";
+const char *names[3] = {"Steven", "Andreas", "noname"}; //all users are stored in this array. Last value is dummy value that returns when std::find() does not find occurence of string.
 
 void setup()
 {
@@ -28,38 +30,52 @@ void setup()
     pinMode(LED_BLUE, OUTPUT);
 }
 
+
 void loop()
 {
-    naam_received = RX_Handler();
-    if (naam_received == "Steven")
+    // naam_received = RX_Handler();
+    naam_received = "Steven"; //dit is gewoon om te simuleren, gebruik lijn hierboven voor echt programma (kweet nie of die functie werkt) 
+    if (naam_received == "unknown")
     {
-        if (stemherkenning() == "Steven")
+        String speech_Name = stemherkenning();
+        String RFID_Name = RFID_Read();
+        if (speech_Name != "unknown" && speech_Name == RFID_Name)
         {
-            //publish steven naar mqtt
+            // send open
+            Serial.print("open");
         }
-        else if (stemherkenning() != "Steven")
+        else
         {
-            rfid_naam = RFID_Read();
+            // send close
+            Serial.print("close");
         }
     }
-    else if (naam_received == "Andreas")
+    else if (*std::find(std::begin(names), std::end(names), naam_received) != "noname")
     {
-        if (stemherkenning() == "Andreas")
+        String speech_Name = stemherkenning();
+        if (speech_Name != "unknown" && speech_Name == naam_received)
         {
-            //publish steven naar mqtt
+            // send open
+            Serial.print("open");
         }
-        else if (stemherkenning() != "Andreas")
+        else
         {
-            rfid_naam = RFID_Read();
+            String RFID_Name = RFID_Read();
+            if (RFID_Name != "unknown" && RFID_Name == naam_received)
+            {
+                // send open
+                Serial.print("open");
+            }
+            else
+            {
+                // send close
+                Serial.print("close");         
+            }
         }
     }
-    else
-    {
-        stem_naam = stemherkenning();
-        rfid_naam = RFID_Read();
-    }
-    String writenames = SOT + STX + stem_naam + ETX + STX + rfid_naam + ETX + EOT;
-    Serial1.print(writenames);
+    // String writenames = SOT + STX + stem_naam + ETX + STX + rfid_naam + ETX + EOT;
+    // Serial1.print(writenames);
+    delay(1000);
 }
 
 String RX_Handler()
@@ -69,23 +85,31 @@ String RX_Handler()
         if (Serial1.readString() == "STX")
             return (Serial1.readStringUntil(ETX));
     }
+    return "Andreas";
 }
 
 String RFID_Read()
 {
-    String naam;
+    //Get RFID tag and wait maybe? return "unkown" if none was read.
+
+    String naam = "unknown";
     return naam;
 }
 
 String stemherkenning()
 {
-    String naam;
+    //get a name (wait a certain amount of time for a name to be read.)
+    //return name if name is read, return something ("unknown?") if not.
 
+    String naam = "Andreas";
     return naam;
 }
 
 void setupComm()
 {
-    Serial1.begin(115200); // Init serial comm
+    Serial.begin(115200); // Init serial comm
+
     SPI.begin();           // Init SPI for card reader
 }
+
+
