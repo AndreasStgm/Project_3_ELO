c1285072f3b33322c6a1cff6182d52308f07a35f
stevenv95 <r0465345@ucll.be>
2021-12-20 16:03:57 +0100

    Hopelijk

---
+++ Arduino Nano 33 BLE Sense Event CTRL/src/SpeechRec.cpp
@@ -0,0 +1,216 @@
+#include <PDM.h>
+#include <Names_inferencing.h>
+
+#define EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW 3
+
+typedef struct
+{
+    signed short *buffers[2];
+    unsigned char buf_select;
+    unsigned char buf_ready;
+    unsigned int buf_count;
+    unsigned int n_samples;
+} inference_t;
+static inference_t inference;
+static bool record_ready = false;
+static signed short *sampleBuffer;
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static int print_results = -(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW);
+/**
+ * @brief      Printf function uses vsnprintf and output using Arduino Serial
+ *
+ * @param[in]  format     Variable argument list
+ */
+void ei_printf(const char *format, ...)
+{
+    static char print_buf[1024] = {0};
+
+    va_list args;
+    va_start(args, format);
+    int r = vsnprintf(print_buf, sizeof(print_buf), format, args);
+    va_end(args);
+
+    if (r > 0)
+    {
+        Serial.write(print_buf);
+    }
+}
+/**
+ * @brief      PDM buffer full callback
+ *             Get data and call audio thread callback
+ */
+static void pdm_data_ready_inference_callback(void)
+{
+    int bytesAvailable = PDM.available();
+
+    // read into the sample buffer
+    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+    if (record_ready == true)
+    {
+        for (int i = 0; i < bytesRead >> 1; i++)
+        {
+            inference.buffers[inference.buf_select][inference.buf_count++] = sampleBuffer[i];
+
+            if (inference.buf_count >= inference.n_samples)
+            {
+                inference.buf_select ^= 1;
+                inference.buf_count = 0;
+                inference.buf_ready = 1;
+            }
+        }
+    }
+}
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples)
+{
+    inference.buffers[0] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[0] == NULL)
+    {
+        return false;
+    }
+
+    inference.buffers[1] = (signed short *)malloc(n_samples * sizeof(signed short));
+
+    if (inference.buffers[1] == NULL)
+    {
+        free(inference.buffers[0]);
+        return false;
+    }
+
+    sampleBuffer = (signed short *)malloc((n_samples >> 1) * sizeof(signed short));
+
+    if (sampleBuffer == NULL)
+    {
+        free(inference.buffers[0]);
+        free(inference.buffers[1]);
+        return false;
+    }
+
+    inference.buf_select = 0;
+    inference.buf_count = 0;
+    inference.n_samples = n_samples;
+    inference.buf_ready = 0;
+
+    // configure the data receive callback
+    PDM.onReceive(&pdm_data_ready_inference_callback);
+
+    PDM.setBufferSize((n_samples >> 1) * sizeof(int16_t));
+
+    // initialize PDM with:
+    // - one channel (mono mode)
+    // - a 16 kHz sample rate
+    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY))
+    {
+        ei_printf("Failed to start PDM!");
+    }
+
+    // set the gain, defaults to 20
+    PDM.setGain(127);
+
+    record_ready = true;
+
+    return true;
+}
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void)
+{
+    bool ret = true;
+    if (inference.buf_ready == 1)
+    {
+        ei_printf(
+            "Error sample buffer overrun. Decrease the number of slices per model window "
+            "(EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW)\n");
+        ret = false;
+    }
+
+    while (inference.buf_ready == 0)
+    {
+        delay(1);
+    }
+
+    inference.buf_ready = 0;
+
+    return ret;
+}
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    numpy::int16_to_float(&inference.buffers[inference.buf_select ^ 1][offset], out_ptr, length);
+
+    return 0;
+}
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void)
+{
+    PDM.end();
+    free(inference.buffers[0]);
+    free(inference.buffers[1]);
+    free(sampleBuffer);
+}
+/**
+ * @brief      Arduino setup function
+ */
+void setupSpeech()
+{
+
+    run_classifier_init();
+    if (microphone_inference_start(EI_CLASSIFIER_SLICE_SIZE) == false)
+    {
+        ei_printf("ERR: Failed to setup audio sampling\r\n");
+        return;
+    }
+}
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+String recognize()
+{
+
+    bool m = microphone_inference_record();
+    if (!m)
+    {
+        ei_printf("ERR: Failed to record audio...\n");
+        return;
+    }
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_SLICE_SIZE;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = {0};
+    EI_IMPULSE_ERROR r = run_classifier_continuous(&signal, &result, debug_nn);
+    if (r != EI_IMPULSE_OK)
+    {
+        ei_printf("ERR: Failed to run classifier (%d)\n", r);
+        return;
+    }
+    if (++print_results >= (EI_CLASSIFIER_SLICES_PER_MODEL_WINDOW))
+    {
+        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++)
+        {
+            ei_printf("    %s: %.5f\n", result.classification[ix].label,
+                      result.classification[ix].value);
+        }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+        ei_printf("    anomaly score: %.3f\n", result.anomaly);
+#endif
+        print_results = 0;
+    }
+}
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
