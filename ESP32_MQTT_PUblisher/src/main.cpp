7f31408b163b1f11e90db0144fd5256648b81447
AndreasStgm <andreassteegmans@hotmail.com>
2021-12-23 14:51:54 +0100

    opkuisen mqtt_publisher en comments engerish

--- ESP32_MQTT_PUblisher/src/main.cpp
+++ ESP32_MQTT_PUblisher/src/main.cpp
@@ -1,69 +1,36 @@
 #include <Arduino.h>
-#include <PubSubClient.h> // connecteer en publish naar MQTT BROKER
-#include <WiFi.h>         // Wifi enabler voor ESP32
+#include <PubSubClient.h> // connect and publish to MQTT BROKER
+#include <WiFi.h>         // Wifi enabler for ESP32
 #include "uart_project.h"
 
 byte receivedData[5] = {0, 0, 0, 0, 0};
 String sendData = "";
 
-// WiFi
-// const char *ssid = "LAPTOP-Paco";            // wifi invullen
-// const char *wifi_password = "Elpolloloco69"; // pw invullen
+const char *ssid = "ijmertnet";         // wifi to fill in
+const char *wifi_password = "computer"; // password to fill in
 
-const char *ssid = "ijmertnet";         // wifi invullen
-const char *wifi_password = "computer"; // pw invullen
+// MQTT user data
+const char *mqtt_server = "192.168.137.134"; // IP of MQTT broker, our case: 192.168.0.13
+const char *slot_topic = "slot";             // MQTT topic over which we will publish
+const char *mqtt_username = "ijmert";        // MQTT username to fill in
+const char *mqtt_password = "ijmert";        // MQTT password to fill in
+const char *clientID = "client_home";        // MQTT client ID to fill in
 
-//const char *ssid = "Orange-6f9d5";         // wifi invullen
-//const char *wifi_password = "Qwcf933c"; // pw invullen
-
-// MQTT Ijmerts
-const char *mqtt_server = "192.168.137.134"; // IP van MQTT broker invullen 192.168.0.13
-const char *slot_topic = "slot";             // home/topic nog in te vullen
-const char *mqtt_username = "ijmert";        // MQTT username invullen
-const char *mqtt_password = "ijmert";        // MQTT pw invullen
-const char *clientID = "client_home";        // MQTT client ID invullen
-
-// MQTT Mathias
-//~ const char *mqtt_server = "192.168.0.13"; // IP van MQTT broker invullen 192.168.0.13
-//~ const char *RFIDtag_topic = "RFIDtag";       // home/topic nog in te vullen
-//~ const char *mqtt_username = "esp32";        // MQTT username invullen
-//~ const char *mqtt_password = "esp32";        // MQTT pw invullen
-//~ const char *clientID = "client_home";        // MQTT client ID invullen
-
-// const char *mqtt_server = "192.168.137.195"; // IP van MQTT broker invullen 192.168.0.13
-// const char *RFIDtag_topic = "RFIDtag";       // home/topic nog in te vullen
-// const char *mqtt_username = "esp32";         // MQTT username invullen
-// const char *mqtt_password = "esp32";         // MQTT pw invullen
-// const char *clientID = "client_home";        // MQTT client ID invullen
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const char *VoiceRecognition_topic = "VoiceRecognition"; // mathias sub_test
 const char *FacialRecognition_topic = "FacialRecognition";
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-char *Topic;
-// byte buffer[15];
-boolean Rflag = false;
-int r_len;
-boolean succes;
-
-// Start Wifi en MQTT
+// Start Wifi and MQTT
 WiFiClient wifiClient;
-// RFIDPayload receivedPayload;
 PubSubClient client(mqtt_server, /*LISTENER PORT BROKER INVULLEN INGEVAL ANDERE*/ 1883, wifiClient);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
+//---------------functions---------------
+
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  //Payload=[];
   UARTPayload receivedPayload;
   strncpy(receivedPayload.facialName, "", 20);
   strncpy(receivedPayload.audioName, "", 20);
   strncpy(receivedPayload.rfidName, "", 20);
 
-  Topic = topic;
-  Rflag = true;   //will use in main loop
-  r_len = length; //will use in main loop
   debugSerial.print("length message received in callback= ");
   debugSerial.println(length);
   for (int i = 0; i < length; i++)
@@ -73,9 +40,8 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
   commsSend(&receivedPayload);
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////
 
-// functie voor met de MQTT te verbinden over wifi
+// function used for connection the esp32 to the wifi and MQTT broker
 void connect_MQTT()
 {
   debugSerial.print("Connecting to ");
@@ -83,7 +49,7 @@ void connect_MQTT()
 
   WiFi.begin(ssid, wifi_password);
 
-  // wachten op connectie
+  // waiting for the connection
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -95,8 +61,8 @@ void connect_MQTT()
   debugSerial.print("IP address: ");
   debugSerial.println(WiFi.localIP());
 
-  // Connect met MQTT Broker
-  // client.connect checkt als de connectie gelukt is
+  // Connects to  MQTT Broker
+  // client.connect checks if the connection is successful
   if (client.connect(clientID, mqtt_username, mqtt_password))
   {
     debugSerial.println("Connected to MQTT Broker!");
@@ -107,49 +73,24 @@ void connect_MQTT()
   }
 }
 
-//-----
+//---------------standard arduino framework stuff---------------
 
 void setup()
 {
   debugSerial.begin(9600);
   commsSerial.begin(115200);
 
-  //client.setServer(mqtt_server, 1883); //test 02/12
   client.setCallback(callback);
   connect_MQTT();
-  // client.subscribe(FacialRecognition_topic,2);
 }
 
 void loop()
 {
   client.connect(clientID, mqtt_username, mqtt_password);
-  succes = client.subscribe(FacialRecognition_topic);
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-  // client.subscribe(FacialRecognition_topic,2);
-  // String msg = "test message";
-  // client.publish(FacialRecognition_topic, String(msg).c_str());
-
-  // delay(1000);
+  boolean success = client.subscribe(FacialRecognition_topic);
+  debugSerial.printf("Subscribe success: %d (true/false)\n", success);
 
   client.loop();
-
-  // if (Rflag)
-  // {
-  //   debugSerial.print("Message arrived in main loop[");
-  //   debugSerial.print(Topic);
-  //   debugSerial.print("] ");
-  //   debugSerial.print("message length = ");
-  //   debugSerial.print(r_len);
-  //   //Serial.print(Payload);
-  //   for (int i = 0; i < r_len; i++)
-  //   {
-  //     debugSerial.print((char)buffer[i]);
-  //   }
-  //   debugSerial.println();
-  //   Rflag = false;
-  // }
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////
 
   if (commsSerial.available() > 0)
   {
@@ -180,17 +121,7 @@ void loop()
     receivedData[2] = 0;
     receivedData[3] = 0;
     receivedData[4] = 0;
-    // UARTPayload receivedPayload = commsRead();
 
-    // String sendDataAudio = "";
-    // String sendDataRfid = "";
-    // sendDataAudio = sendDataAudio + receivedPayload.audioName;
-    // sendDataRfid = sendDataRfid + receivedPayload.rfidName;
-
-    // data check
-    // debugSerial.printf("Data: audio_recogn: %s, rfid_tag: %s\n", sendDataAudio, sendDataRfid);
-
-    // PUBLISH naar MQTT Broker (topic = Validation)
     if (client.publish(slot_topic, String(sendData).c_str()))
     {
       delay(100);
@@ -200,22 +131,9 @@ void loop()
     {
       debugSerial.println("Slot data failed to send. Reconnecting to MQTT Broker and trying again\n");
       client.connect(clientID, mqtt_username, mqtt_password);
-      delay(10); // Zorgt ervoor dat client.publish en client.connect niet botsen
+      delay(10); // Is nedded so that client.connect and client.publish don't collide
       client.publish(slot_topic, String(sendData).c_str());
     }
-    // if (client.publish(VoiceRecognition_topic, String(sendDataAudio).c_str()))
-    // {
-    //   delay(100);
-    //   debugSerial.println("Audio data sent!");
-    // }
-    // else
-    // {
-    //   debugSerial.println("Audio data failed to send. Reconnecting to MQTT Broker and trying again\n");
-    //   client.connect(clientID, mqtt_username, mqtt_password);
-    //   delay(10); // Zorgt ervoor dat client.publish en client.connect niet botsen
-    //   client.publish(VoiceRecognition_topic, String(sendDataAudio).c_str());
-    // }
-    //Als het niet lukt krijgen we volgende melding en probeert hij opnieuw
 
     client.disconnect(); // disconnect MQTT broker
     delay(10);
--- ESP32_MQTT_PUblisher/src/uart_project.cpp
+++ ESP32_MQTT_PUblisher/src/uart_project.cpp
@@ -3,7 +3,7 @@
 
 #define DEBUG
 
-byte readData[68]; // maximaal aantal bytes dat het bericht kan bevatten
+byte readData[68]; // maximum amount of that the uart message can consist of
 
 UARTPayload commsRead()
 {
